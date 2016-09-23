51ff2174ef2d9e2850af19fca5ff04d9c3463abb
Evan Sparks <sparks@cs.berkeley.edu>
2016-09-23 14:14:35 -0700

    Adding example executor for newsgroups.

---
+++ examples/newsgroups.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TRAINPATH=/Users/sparks/datasets/20news-bydate-train
+TESTPATH=/Users/sparks/datasets/20news-bydate-test
+
+LOGDIR=`pwd`/log
+
+if [ ! -d $LOGDIR ]
+then
+	mkdir -p $LOGDIR
+fi
+
+DATE=`date +%Y%m%d.%H%M%S`
+
+#Run the pipeline
+KEYSTONE_MEM=4g ./bin/run-pipeline.sh \
+  pipelines.text.NewsgroupsTuningPipeline \
+  --trainLocation $TRAINPATH \
+  --testLocation $TESTPATH > $LOGDIR/newsgroups.$DATE.log
